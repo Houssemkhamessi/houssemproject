867b535da2ff4e801459faceb5bc2568b8174c14
Houssemkhamessi <hkhamessi87@gmail.com>
2023-11-28 16:37:43 +0100

    sdfgh

---
+++ ball.js
@@ -0,0 +1,20 @@
+export default class ball {
+    constructor(ballElem) {
+        this.ballElem = ballElem
+    }
+
+    get x(){
+      return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
+    }
+
+    set x(value) {
+      this.ballElem.style.setProperty("--x", value)
+    }
+
+ 
+ update(delta) {
+   this.x = 5
+ }
+
+
+}
--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+function rolldice() {
 
+    const numofDice = document.getElementById("numofDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+
+    for(let i = 0; i < numofDice; i++) {
+        const value = Math.floor(Math.random() * 6) +1;
+        console.log(value);
+        values.push(value);
+        images.push(`<img src="./dice-images/Dice-${value}.png">`);
+
+
+    }
+
+    diceResult.textContent = `dice: ${values.join(', ')} ` ;
+    diceImages.innerHTML = images.join('');
+}
